6099f1106d81d650ba75938a9504bfbdc2c5bf42
Agne Urbaityte <agne.urbaityte@gmail.com>
2020-11-28 21:36:24 -0500

    Redux files in the store making axios calls to our country and states api's

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,16 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import countryReducer from './country'
+import stateJobsReducer from './stateJobs'
+import stateTotalsReducer from './stateTotals'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user,
+  country: countryReducer,
+  stateJobs: stateJobsReducer,
+  stateTotals: stateTotalsReducer
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +20,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './country'
---
+++ client/store/stateJobs.js
@@ -0,0 +1,30 @@
+import Axios from 'axios'
+
+const SET_STATE_JOBS = 'SET_STATE_JOBS'
+
+export const setStateJobs = stateJobs => ({
+  type: SET_STATE_JOBS,
+  stateJobs
+})
+
+export const fetchStateJobs = (state = 'New-York', filter = 'Javascript') => {
+  return async dispatch => {
+    try {
+      const {data} = await Axios.get(`/api/state/${state}/jobs/${filter}`)
+      dispatch(setStateJobs(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+const initialState = {}
+
+export default function stateJobsReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_STATE_JOBS:
+      return action.stateJobs
+    default:
+      return state
+  }
+}
