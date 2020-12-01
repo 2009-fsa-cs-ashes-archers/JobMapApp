19f2188d0a0a651d2c7b7ae69a7908cac86de592
Danny Berger <danberadi@gmail.com>
2020-11-30 23:16:45 -0500

    regex added

--- server/api/state.js
+++ server/api/state.js
@@ -5,47 +5,49 @@ const jobDataHelper = require('./jobDataHelper')
 
 module.exports = router
 
-const key =
-  // For 2-word states and multiple filters, please submit them to this route with each word separated by a '-'
-  // For example: 'New-Jersey/jobs/react-redux-node
+const key = 'PLACEHOLDER'
+// For 2-word states and multiple filters, please submit them to this route with each word separated by a '-'
+// For example: 'New-Jersey/jobs/react-redux-node
 
-  // ** The two routes below can be combined to return state-wide information AND a list of jobs **
+// ** The two routes below can be combined to return state-wide information AND a list of jobs **
 
-  // Dynamic State Jobs -- * USE THIS ROUTE *
-  router.get('/:state/jobs/:filter', async (req, res, next) => {
-    try {
-      const state = req.params.state.split('-').join('%20')
-      // Please Pass in 'Javascript' to req.params.filter if user leaves field empty
-      const filter = req.params.filter.split('-').join('%20')
-      const {data} = await axios.get(
-        `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=bc9f8e70&app_key=83d35d0e2fa37d07733767a7b28952ca&what_and=${filter}&what_or=software%20developer%20engineer%20web%20javascript%20full%20stack&location0=US&location1=${state}&max_days_old=30&sort_by=relevance`
-      )
+// Dynamic State Jobs -- * USE THIS ROUTE *
+router.get('/:state/jobs/:filter', async (req, res, next) => {
+  try {
+    const state = req.params.state.split('-').join('%20')
+    // Please Pass in 'Javascript' to req.params.filter if user leaves field empty
+    const filter = req.params.filter.split('-').join('%20')
+    const {data} = await axios.get(
+      `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=bc9f8e70&app_key=83d35d0e2fa37d07733767a7b28952ca&what_and=${filter}&what_or=software%20developer%20engineer%20web%20javascript%20full%20stack&location0=US&location1=${state}&max_days_old=30&sort_by=relevance`
+    )
 
-      console.log('returns ' + data.results.length + ' jobs')
-      for (let i = 0; i < data.results.length; i++) {
-        if (data.results[i].location.area.length < 3) {
-          const name = data.results[i].company.display_name
-            .split(' ')
-            .join('%20')
-          console.log('run google place API search on this one for', name)
-          // const {data} = await axios.get(`
-          //   https://maps.googleapis.com/maps/api/place/findplacefromtext/json?${key}&${data.results[i].company.display_name}
-          // `)
-          // console.log("new data for this^", data)
-        }
+    console.log('returns ' + data.results.length + ' jobs')
+    for (let i = 0; i < data.results.length; i++) {
+      if (data.results[i].location.area.length < 3) {
+        const name = data.results[i].company.display_name
+          .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+          .split(' ')
+          .join('%20')
+        // regex = '/[a-z]/i'
+        console.log('run google place API search on this one for', name)
+        // const {data} = await axios.get(`
+        //   https://maps.googleapis.com/maps/api/place/findplacefromtext/json?${key}&${data.results[i].company.display_name}
+        // `)
+        // console.log("new data for this^", data)
       }
-
-      const jobs = jobDataHelper(data.results)
-      res.json({
-        count: data.count,
-        // Depeneding on the specificity of the search, none of the results might have a salary attached, and this field will be undefined
-        averageSalary: data.mean,
-        jobs: jobs
-      })
-    } catch (err) {
-      next(err)
     }
-  })
+
+    const jobs = jobDataHelper(data.results)
+    res.json({
+      count: data.count,
+      // Depeneding on the specificity of the search, none of the results might have a salary attached, and this field will be undefined
+      averageSalary: data.mean,
+      jobs: jobs
+    })
+  } catch (err) {
+    next(err)
+  }
+})
 
 // Next route could be called when a user clicks on a specific state to get more thorough info on that state, but before making the above api call for jobs
 
