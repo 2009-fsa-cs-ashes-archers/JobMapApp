072feeebf4d8f4908ccbdc996294c0a3061e5453
Peter Boustani <boustanip718@gmail.com>
2020-12-03 11:51:45 -0500

    Can change filter and state and holds their value

--- client/components/Filter.js
+++ client/components/Filter.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import {states} from '../../utils/constants'
 
 const Filter = props => {
-  return <h4>Filter</h4>
+  return (
+    <div className="filter">
+      <h4>Filter</h4>
+      <label>
+        Keywords:
+        <input
+          type="text"
+          name="keyword"
+          onChange={e => props.handleFilter(e.target.value)}
+          value={props.keyword}
+        />
+      </label>
+      <label>
+        Choose State:
+        <select
+          value={props.state}
+          onChange={e => props.handleState(e.target.value)}
+        >
+          {states.map((state, i) => (
+            <option key={i} value={state}>
+              {state}
+            </option>
+          ))}
+        </select>
+      </label>
+      <button onClick={props.dealWithSubmit}>Apply Filters</button>
+    </div>
+  )
 }
 
 export default Filter
--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,15 +1,84 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Filter, SearchData} from '../components'
 
 const Sidebar = props => {
+  const [filter, setFilter] = useState(props.filter || '')
+  const [geoState, setGeoState] = useState(props.selectedState || 'New York')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(filter)
+    console.log(geoState)
+  }
+
+  // useEffect(() => {
+  //   console.log(filter)
+  // }, [filter])
+
+  // useEffect(() => {
+  //   console.log(geoState)
+  // }, [geoState])
+
   return (
     <div id="sidebar-container">
-      <Filter />
+      <Filter
+        keyword={filter}
+        handleFilter={setFilter}
+        state={geoState}
+        handleState={setGeoState}
+        dealWithSubmit={handleSubmit}
+      />
       <SearchData />
     </div>
   )
 }
 
-export default Sidebar
+const mapStateToProps = state => {
+  return {
+    filter: state.filter
+  }
+}
+
+export default connect(mapStateToProps)(Sidebar)
+
+// export class Sidebar extends React.Component {
+//   constructor(props) {
+//     super()
+//     this.state = {
+//       keyword: '',
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//   }
+
+//   handleChange(event) {
+//     this.setState({
+//       [event.target.name]: event.target.value,
+//     })
+//     console.log(this.state)
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault()
+//     console.log(this.state.keyword)
+//     this.setState({
+//       keyword: '',
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div id="sidebar-container">
+//         <Filter
+//           handleChange={this.handleChange}
+//           handleSubmit={this.handleSubmit}
+//         />
+//         <SearchData />
+//       </div>
+//     )
+//   }
+// }
+
+// export default Sidebar
